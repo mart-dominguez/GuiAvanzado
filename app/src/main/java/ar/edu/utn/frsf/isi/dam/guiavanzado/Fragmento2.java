28d2e17410ad233fab45563df4e9fba5bd4391de
mart-dominguez <mdomingu@gmail.com>
2017-10-04 23:51:55 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/guiavanzado/Fragmento2.java
@@ -0,0 +1,28 @@
+package ar.edu.utn.frsf.isi.dam.guiavanzado;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragmento2 extends Fragment {
+
+
+    public Fragmento2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragmento2, container, false);
+    }
+
+}
---
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/guiavanzado/MainActivity.java
@@ -0,0 +1,64 @@
+package ar.edu.utn.frsf.isi.dam.guiavanzado;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] opciones;
+    private ListView listaMenu;
+    private DrawerLayout miDrawerLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        opciones = getResources().getStringArray(R.array.opcionesMnu);
+        listaMenu = (ListView) findViewById(R.id.left_drawer);
+        miDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Set the adapter for the list view
+        listaMenu.setAdapter(new ArrayAdapter<>(this,
+                R.layout.drawer_list_item
+                , opciones));
+        // Set the list's click listener
+        listaMenu.setOnItemClickListener(clickMenu);
+    }
+    private ListView.OnItemClickListener clickMenu = new ListView.OnItemClickListener (){
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            elegirFragmento(position);
+        }
+    };
+
+
+    /** Swaps fragments in the main content view */
+    private void elegirFragmento(int position) {
+
+        Fragment fragment =null;
+        // Create a new fragment and specify the planet to show based on position
+       if(position%2==0) fragment = new Fragmento1();
+        else fragment = new Fragmento2();
+       // Bundle args = new Bundle();
+        //args.putInt(Fragmento1.ARG_PLANET_NUMBER, position);
+        //fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        listaMenu.setItemChecked(position, true);
+        setTitle(opciones[position]);
+        miDrawerLayout.closeDrawer(listaMenu);
+    }
+}
